09b427294774e69d08cc54280c2c9ea67c6f81c9
Elias Giannakidis <hl.giannakidis@gmail.com>
2023-04-10 13:34:10 +0300

    Add files via upload

---
+++ testJson.py
@@ -0,0 +1,17 @@
+data = {
+    "Joe": {
+        "math": 65,
+        "science": 78,
+        "english": 98
+    },
+    "Bill": {
+        "math": 55,
+        "science": 72,
+        "english": 87
+    },
+    "Jane": {
+        "math": 100,
+        "science": 100,
+        "english": 90
+    }
+}
